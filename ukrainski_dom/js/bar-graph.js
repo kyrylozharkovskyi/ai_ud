e28f4902ece17618227b4b8baf69778b665e07e5
Your Name <kyrylo.zharkovskyi@gmail.com>
2024-04-12 19:19:22 +0200

    new5

--- ukrainski_dom/js/bar-graph.js
+++ ukrainski_dom/js/bar-graph.js
@@ -3,7 +3,7 @@ let bars = {};
 let graphWrapper;
 
 // these are the colors of our bars
-let colors = ['#84d025', '#feed24', '#343112', '#1291D0'];
+let colors = ['#155201', '#feed24', '#343112', '#1291D0'];
 let lightColors = ['#FFECE2', '#FFE9EC', '#F1F0FF', '#E2F5FF'];
 
 // This function makes the bar graph
