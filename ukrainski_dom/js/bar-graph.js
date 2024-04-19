128048dd12d3d28fc35ea72baccc84e42d647c40
Your Name <kyrylo.zharkovskyi@gmail.com>
2024-04-19 23:54:37 +0200

    Update bar-graph.js

--- ukrainski_dom/js/bar-graph.js
+++ ukrainski_dom/js/bar-graph.js
@@ -72,7 +72,7 @@ export function updateBarGraph(data) {
         percentElement.innerText = convertToPercent(probability);
 
         // Check if the className is 'class1' and probability is greater than 0.9
-        if (className === 'No Banana' && probability >= 0.999) {
+        if (className === 'Стиглий' && probability >= 0.999) {
             let audio = new Audio('./sound/Bruh.mp3');
             audio.play();
         }
